183099d7d1cb217d1b2529d663a7f141985109ce
DmApostolov <apostolovdm@yandex.ru>
2021-02-22 18:34:10 +0300

    Third Dmitry's version. Hope didn't broke anything of yours

--- api/views/user_viewset.py
+++ api/views/user_viewset.py
@@ -16,66 +16,49 @@ import random
 import string
 
 
-class UserViewSet(viewsets.ModelViewSet):
-
-    serializer_class = UserSerializer
-    lookup_field = 'username'
-
-    def get_queryset(self):
-        queryset = User.objects.all()
-        return queryset
-
-    def get_permissions(self):
-        if self.action == 'create' or self.action == 'perform_create':
-            permission_classes = [IsHimselfOrSuperUser]
-        elif self.action == 'list' or self.action == 'retrieve':
-            permission_classes = [permissions.IsAuthenticated]
-        elif self.action == 'destroy' or self.action == 'update' or self.action == 'partial_update':
-            permission_classes = [permissions.IsAuthenticated]
-        else:
-            permission_classes = [IsAdmin]
-        return [permission() for permission in permission_classes]
-
-
-class MyUserViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
-    serializer_class = UserSerializer
-
-    def get_queryset(self):
-        return User.objects.filter(username=self.request.user.username)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset[0])
-        return Response(serializer.data)
-
-    def partial_update(self, request, *args, **kwargs):
-
-        queryset = self.list()
-        serializer = UserSerializer(queryset, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-
 def mail_confirm(request):
+    global conf_code
+    global email_to
+    email_to = request.POST['email']
     if request.method == "POST":
         letters = string.ascii_lowercase
-        result_str = ''.join(random.choice(letters) for i in range(6))
-        return send_mail(
+        conf_code = ''.join(random.choice(letters) for i in range(6))
+        email = EmailMessage(
             'Conformation code',
-            f'Your conformation code for authentification is {result_str}.',
+            f'Your conformation code for authentification is {conf_code}.',
             'apostolovdm@gmail.com',
-            request.POST['email'],
-            fail_silently=False,
+            email_to
         )
+        return email.send(fail_silently=False)
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
-    def get_serializer_class(self):
-        if ("email" in self.request.data) and ("confirmation_code" in self.request.data):
-            # if (confirmation_code == confirmation_code):
-            return MyTokenObtainPairSerializer
-        res = {
-            'error': 'can not authenticate with the given credentials or the account has been deactivated'
-        }
-        return Response(res, status=status.HTTP_403_FORBIDDEN)
+    def get_serializer_class(self, request):
+        if ('email' in request.data) and ('confirmation_code' in request.data):
+            if request.data['confirmation_code'] == conf_code:
+                return MyTokenObtainPairSerializer
+        return TokenObtainPairSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdmin]
+    pagination_class = PageNumberPagination
+    lookup_field = 'username'
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    @action(
+        detail=False,
+        methods=['GET', 'PATCH'],
+        permission_classes=[IsAuthenticated]
+    )
+    def me(self, request, *args, **kwargs):
+        self.object = get_object_or_404(User, username=request.user.username)
+        serializer = self.get_serializer(
+            self.object,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
